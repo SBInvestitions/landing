5574a35f6023f0a72bea46e5ed623abb384577ab
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2018-03-06 18:03:15 +0300

    landing in progress

---
+++ src/components/FiveStones/FiveStones.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="top-header" class="five-stones">
+    <el-row type="flex" class="row-bg" justify="space-around">
+      <el-col :span="3">
+        <div class="grid-content bg-purple">
+          <div class="stone-block stone-block1">
+            <div class="stone"></div>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="grid-content bg-purple-light">
+          <div class="stone-block stone-block2">
+            <div class="stone"></div>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="grid-content bg-purple">
+          <div class="stone-block stone-block3">
+            <div class="stone"></div>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="grid-content bg-purple">
+          <div class="stone-block stone-block4">
+            <div class="stone"></div>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="grid-content bg-purple">
+          <div class="stone-block stone-block5">
+            <div class="stone"></div>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<style lang="scss" src="./style.scss" scoped></style>
+
+<script>
+  export default {
+    name: 'FiveStones'
+  };
+</script>
--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -3,7 +3,7 @@
     <el-header>
       <el-row type="flex" class="row-bg menu-row" justify="center">
         <el-menu :default-active="activeIndex" class="el-menu-sbi" mode="horizontal" @select="handleSelect">
-          <el-menu-item index="1">SBInvestments</el-menu-item>
+          <el-menu-item index="1">SBInvestitions</el-menu-item>
           <el-menu-item index="2">About Project</el-menu-item>
           <el-menu-item index="3">DAICO conditions</el-menu-item>
           <el-menu-item index="4">Roadmap</el-menu-item>
@@ -12,7 +12,7 @@
           <el-menu-item index="7">Clients</el-menu-item>
           <el-menu-item index="8">FAQ</el-menu-item>
           <!-- WHAT IS DAICO https://cointelegraph.com/explained/what-is-a-daico-explained -->
-          <el-submenu index="9">
+          <el-submenu index="9" class="el-sub-menu-sbi">
             <template slot="title">Docs</template>
             <el-menu-item index="9-1">Whitepaper</el-menu-item>
             <el-menu-item index="9-2">Business Summary</el-menu-item>
@@ -24,7 +24,89 @@
       <top-header/>
     </el-header>
     <el-main>
-      Main
+      <five-stones/>
+      <el-row class="about-company">
+        <el-col :span="24" class="top-bottom">
+          <el-row type="flex" class="row-bg" justify="space-around">
+            <el-col :span="16">
+              <div class="grid-content bg-purple-light">
+                <el-row type="flex" class="row-bg" justify="space-around">
+                  <el-col :span="24">
+                    <div class="grid-content bg-purple-light">
+                      <h3>О проекте</h3>
+                      <div class="text-block">
+                        <div class="text">
+                          <p>Мы располагаем геологической информацией об участках недр в Суоярвском районе Республики Карелия
+                          с ресурсами залегания блочного камня в количестве 4 штук с ориентировочными запасами блоков 100 000 м3 каждый.
+                          Такого запаса хватит на 40-50 лет разработки.</p>
+                          <p>Для подготовки данных месторождений к разработке, оформления лицензий и последующей продажи добывающим компаниям мы привлекаем средства инвесторов.</p>
+                        </div>
+                      </div>
+                    </div>
+                  </el-col>
+                </el-row>
+
+                <h3>Образцы камня</h3>
+                <el-row :gutter="12" class="granit">
+                  <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple"></div></el-col>
+                  <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">
+                    <div class="grid-content bg-purple-light">
+                      <p>Красный гранит</p>
+                      <img src="./../../../src/assets/red-granit.png" alt="red granit">
+                    </div>
+                  </el-col>
+                  <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">
+                    <div class="grid-content bg-purple">
+                      <p>Черный гранит</p>
+                      <img src="./../../../src/assets/black-granit.png" alt="black granit">
+                    </div>
+                  </el-col>
+                  <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple-light"></div></el-col>
+                </el-row>
+
+                <h3>Применение камня</h3>
+                <div class="text-block">
+                  <div class="text">
+                    <p>
+                      Поскольку камень, найденный нашими специалистами, высокого качества и востребованных цветов,
+                      стоимость продажи каждого месторождения на рынке на сегодняшний день превышает 100 миллионов рублей.
+                    </p>
+                  </div>
+                </div>
+                <el-row type="flex" class="row-bg granit-samples" justify="space-around">
+                  <el-col :span="6">
+                    <div class="grid-content bg-purple">
+                      <img src="./../../../src/assets/i1.png" alt="granit sample 1">
+                    </div>
+                  </el-col>
+                  <el-col :span="6">
+                    <div class="grid-content bg-purple-light">
+                      <img src="./../../../src/assets/i2.png" alt="granit sample 2">
+                    </div>
+                  </el-col>
+                  <el-col :span="6">
+                    <div class="grid-content bg-purple">
+                      <img src="./../../../src/assets/i3.png" alt="granit sample 3">
+                    </div>
+                  </el-col>
+                  <el-col :span="6">
+                    <div class="grid-content bg-purple">
+                      <img src="./../../../src/assets/i4.png" alt="granit sample 4">
+                    </div>
+                  </el-col>
+                </el-row>
+                <div class="text-block">
+                  <div class="text">
+                    <p>
+                      Более подробную информацию вы можете прочитать в нашем <a href="https://sbinvest.pro/docs/white-paper.ru.pdf">Коммерческом предложении</a>.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </el-col>
+          </el-row>
+        </el-col>
+      </el-row>
     </el-main>
     <el-footer>Footer</el-footer>
   </el-container>
@@ -34,6 +116,8 @@
 
 <script>
   import TopHeader from './../TopHeader/TopHeader.vue';
+  import FiveStones from './../FiveStones/FiveStones.vue';
+
   export default {
     name: 'Home',
     props: {
@@ -51,7 +135,8 @@
       }
     },
     components: {
-      'top-header': TopHeader
+      'top-header': TopHeader,
+      'five-stones': FiveStones
     }
   };
 </script>
