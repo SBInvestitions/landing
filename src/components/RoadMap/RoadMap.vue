d8b95c4fa1101370ff01087382559afcb79153ac
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2018-05-09 23:26:57 +0300

    new design

--- src/components/RoadMap/RoadMap.vue
+++ src/components/RoadMap/RoadMap.vue
@@ -12,8 +12,7 @@
           </el-col>
         </el-row>
         <el-row type="flex" class="row-bg stages-middle-line" justify="space-around"></el-row>
-        <el-row type="flex" class="row-bg road-map-stages" justify="space-around">
-
+        <el-row type="flex" class="row-bg road-map-stages hidden-md-and-down" justify="space-around">
           <el-col class="stage" :xs="8" :sm="8" :md="8" :lg="8" :xl="6">
             <div class="number">1</div>
             <div class="round"></div>
@@ -26,7 +25,6 @@
             </div>
             <div class="price">1 SBI = $0.02</div>
           </el-col>
-
           <el-col class="stage" :xs="8" :sm="8" :md="8" :lg="8" :xl="6">
             <div class="number">2</div>
             <div class="round"></div>
@@ -39,7 +37,6 @@
             </div>
             <div class="price">1 SBI = $0.052</div>
           </el-col>
-
           <el-col class="stage" :xs="8" :sm="8" :md="8" :lg="8" :xl="6">
             <div class="number">3</div>
             <div class="round"></div>
@@ -52,7 +49,6 @@
             </div>
             <div class="price">1 SBI = $0.053</div>
           </el-col>
-
           <el-col class="stage" :xs="8" :sm="8" :md="8" :lg="8" :xl="6">
             <div class="number">4</div>
             <div class="round"></div>
@@ -65,7 +61,6 @@
             </div>
             <div class="price">1 SBI = $0.21</div>
           </el-col>
-
           <el-col class="stage" :xs="8" :sm="8" :md="8" :lg="8" :xl="6">
             <div class="number">5</div>
             <div class="round"></div>
@@ -78,7 +73,6 @@
             </div>
             <div class="price">1 SBI = $0.24</div>
           </el-col>
-
           <el-col class="stage" :xs="8" :sm="8" :md="8" :lg="8" :xl="6">
             <div class="number">6</div>
             <div class="round"></div>
@@ -91,7 +85,162 @@
             </div>
             <div class="price">1 SBI = $0.27</div>
           </el-col>
+        </el-row>
 
+        <el-row type="flex" class="row-bg road-map-stages hidden-sm-and-down" justify="space-around">
+          <el-col class="stage" :xs="8" :sm="8" :md="8" :lg="8" :xl="6">
+            <div class="number">1</div>
+            <div class="round"></div>
+            <div class="vertical-line"></div>
+            <div class="header">
+              До 30 июня 2018 года
+            </div>
+            <div class="description">
+              PreICO со скидкой 60%
+            </div>
+            <div class="price">1 SBI = $0.02</div>
+          </el-col>
+          <el-col class="stage" :xs="8" :sm="8" :md="8" :lg="8" :xl="6">
+            <div class="number">2</div>
+            <div class="round"></div>
+            <div class="vertical-line"></div>
+            <div class="header">
+              До 31 августа 2018 года
+            </div>
+            <div class="description">
+              ICO
+            </div>
+            <div class="price">1 SBI = $0.052</div>
+          </el-col>
+          <el-col class="stage" :xs="8" :sm="8" :md="8" :lg="8" :xl="6">
+            <div class="number">3</div>
+            <div class="round"></div>
+            <div class="vertical-line"></div>
+            <div class="header">
+              До 31 января 2019 года
+            </div>
+            <div class="description">
+              Получение лицензий на разведку
+            </div>
+            <div class="price">1 SBI = $0.053</div>
+          </el-col>
+        </el-row>
+        <el-row type="flex" class="row-bg road-map-stages hidden-sm-and-down" justify="space-around">
+          <el-col class="stage" :xs="8" :sm="8" :md="8" :lg="8" :xl="6">
+            <div class="number">4</div>
+            <div class="round"></div>
+            <div class="vertical-line"></div>
+            <div class="header">
+              До 31 июля 2020 года
+            </div>
+            <div class="description">
+              Защита запасов блочного камня
+            </div>
+            <div class="price">1 SBI = $0.21</div>
+          </el-col>
+          <el-col class="stage" :xs="8" :sm="8" :md="8" :lg="8" :xl="6">
+            <div class="number">5</div>
+            <div class="round"></div>
+            <div class="vertical-line"></div>
+            <div class="header">
+              До 30 июня 2021 года
+            </div>
+            <div class="description">
+              Предпродажная подготовка
+            </div>
+            <div class="price">1 SBI = $0.24</div>
+          </el-col>
+          <el-col class="stage" :xs="8" :sm="8" :md="8" :lg="8" :xl="6">
+            <div class="number">6</div>
+            <div class="round"></div>
+            <div class="vertical-line"></div>
+            <div class="header">
+              до 31 декабря 2021 года
+            </div>
+            <div class="description">
+              Продажа месторождений и обратный выкуп SBI
+            </div>
+            <div class="price">1 SBI = $0.27</div>
+          </el-col>
+        </el-row>
+
+        <el-row type="flex" class="row-bg road-map-stages hidden-xs-only" justify="space-around">
+          <el-col class="stage" :xs="8" :sm="8" :md="8" :lg="8" :xl="6">
+            <div class="number">1</div>
+            <div class="round"></div>
+            <div class="vertical-line"></div>
+            <div class="header">
+              До 30 июня 2018 года
+            </div>
+            <div class="description">
+              PreICO со скидкой 60%
+            </div>
+            <div class="price">1 SBI = $0.02</div>
+          </el-col>
+          <el-col class="stage" :xs="8" :sm="8" :md="8" :lg="8" :xl="6">
+            <div class="number">2</div>
+            <div class="round"></div>
+            <div class="vertical-line"></div>
+            <div class="header">
+              До 31 августа 2018 года
+            </div>
+            <div class="description">
+              ICO
+            </div>
+            <div class="price">1 SBI = $0.052</div>
+          </el-col>
+        </el-row>
+        <el-row type="flex" class="row-bg road-map-stages hidden-xs-only" justify="space-around">
+          <el-col class="stage" :xs="8" :sm="8" :md="8" :lg="8" :xl="6">
+            <div class="number">3</div>
+            <div class="round"></div>
+            <div class="vertical-line"></div>
+            <div class="header">
+              До 31 января 2019 года
+            </div>
+            <div class="description">
+              Получение лицензий на разведку
+            </div>
+            <div class="price">1 SBI = $0.053</div>
+          </el-col>
+          <el-col class="stage" :xs="8" :sm="8" :md="8" :lg="8" :xl="6">
+            <div class="number">4</div>
+            <div class="round"></div>
+            <div class="vertical-line"></div>
+            <div class="header">
+              До 31 июля 2020 года
+            </div>
+            <div class="description">
+              Защита запасов блочного камня
+            </div>
+            <div class="price">1 SBI = $0.21</div>
+          </el-col>
+        </el-row>
+        <el-row type="flex" class="row-bg road-map-stages hidden-xs-only" justify="space-around">
+          <el-col class="stage" :xs="8" :sm="8" :md="8" :lg="8" :xl="6">
+            <div class="number">5</div>
+            <div class="round"></div>
+            <div class="vertical-line"></div>
+            <div class="header">
+              До 30 июня 2021 года
+            </div>
+            <div class="description">
+              Предпродажная подготовка
+            </div>
+            <div class="price">1 SBI = $0.24</div>
+          </el-col>
+          <el-col class="stage" :xs="8" :sm="8" :md="8" :lg="8" :xl="6">
+            <div class="number">6</div>
+            <div class="round"></div>
+            <div class="vertical-line"></div>
+            <div class="header">
+              до 31 декабря 2021 года
+            </div>
+            <div class="description">
+              Продажа месторождений и обратный выкуп SBI
+            </div>
+            <div class="price">1 SBI = $0.27</div>
+          </el-col>
         </el-row>
       </el-col>
     </el-row>
