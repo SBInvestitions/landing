6d78d4119e911f79015520fc8050ae06f630647e
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2018-08-07 17:32:47 +0300

    Google page translator

--- src/components/Bounty/Bounty.vue
+++ src/components/Bounty/Bounty.vue
@@ -6,7 +6,7 @@
           <el-col :xs="24" :sm="24" :md="20" :lg="16" :xl="16">
             <div class="grid-content bg-purple-light">
               <h2>{{ $t("bounty.text.1") }}</h2>
-              <p>{{ $t("bounty.text.2") }} <span>3.200.000,00 SBI</span></p>
+              <!--<p>{{ $t("bounty.text.2") }} <span>3.200.000,00 SBI</span></p>
               <span class="round"></span>
               <p>{{ $t("bounty.text.3") }} <span>1.000.000,00 SBI</span> {{ $t("bounty.text.4") }}</p>
               <span class="round"></span>
@@ -14,7 +14,10 @@
               <p><span>1.000.000,00 SBI</span></p>
               <span class="round"></span>
               <p><span>{{ $t("bounty.text.7") }} 2.000.000,00 SBI</span></p>
-              <p>{{ $t("bounty.text.8") }}</p>
+              <p>{{ $t("bounty.text.8") }}</p>-->
+              <p>For those who want to participate in the promotion of our project (reposts, links, attracting customers), we are developing a Bounty program.</p>
+              <p>The holding of Bounty is allocated 3,200,000.00 SBI</p>
+              <p>Those who want to participate in the bounty program send requests to <a href="mailto:bounty@sbinvest.pro">bounty@sbinvest.pro</a></p>
             </div>
           </el-col>
         </el-row>
